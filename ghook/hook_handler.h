038fd9630c5c8a76892d2fef8f46ecd5cbcf5c9b
sbb <sun888486@qq.com>
2022-10-15 15:53:27 +0800

    移除handler中的print，printf在某些情况下会造成crash,加锁也没用，说明不是多线程方面的问题

--- ghook/hook_handler.h
+++ ghook/hook_handler.h
@@ -6,19 +6,16 @@
 
 extern "C" void hook_handler_asm();
 
-std::mutex m1;
 // 这个函数必须多线程安全
 extern "C" void hook_handler_c(minictx * context) {
 	assert(context);
 
-	std::lock_guard<std::mutex> lg(m1);
-
-	printf("[+]hook_handler \n");
-	printf("[+]ori function pointer %p\n", context->OriFunctionPointer);
+	//printf("[+]hook_handler \n");
+	//printf("[+]ori function pointer %p\n", context->OriFunctionPointer);
 
 	if (context->OriFunctionPointer == NtCreateFile) {
 		POBJECT_ATTRIBUTES r8 = (POBJECT_ATTRIBUTES)context->R8;
-		printf("%ws\n", r8->ObjectName->Buffer);			//unsafe
+		//printf("%ws\n", r8->ObjectName->Buffer);			//unsafe
 	}
 }
 
